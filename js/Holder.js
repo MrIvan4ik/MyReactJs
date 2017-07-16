855c2e1a7c33530018dd1c8f5c0be99ab703293d
Unknown <Ivan4ik228@rambler.ru>
2017-07-16 21:46:45 +0300

    Первый коммит

---
+++ js/Holder.js
@@ -0,0 +1,20 @@
+var Holder = React.createClass({
+
+  render: function() {
+    return (
+      <div className="product-menu">
+      <Items title="FOOTBALL SHOES" description="Lorem ipsum dolor sit amet, consectetur adipiscing, vivamus congue nulla leo, quis imperdiet magna.">
+
+       </Items>
+     <Items active="true" title="MODERN CLOTHES" description="Lorem ipsum dolor sit amet, consectetur adipiscing, vivamus congue nulla leo, quis imperdiet magna.">
+
+      </Items>
+      <Items title="BAGS & ACCESSORIES" description="Lorem ipsum dolor sit amet, consectetur adipiscing, vivamus congue nulla leo, quis imperdiet magna.">
+
+       </Items>
+      </div>
+    );
+  }
+});
+
+export {Holder}
---
+++ js/Image.js
@@ -0,0 +1,8 @@
+var Image = React.createClass({
+  render: function() {
+    return (
+      <div className="product-menu-item__image"></div>
+    );
+  }
+});
+module.exports = Image
